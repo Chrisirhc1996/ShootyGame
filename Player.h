ee52e14f824f70b9214303dff33956e7f02fd081
Neil Osbaldeston <osbaldestonneil@gmail.com>
2017-01-23 14:34:31 +0000

    Update Player.h

--- Player.h
+++ Player.h
@@ -6,13 +6,26 @@ class CPlayer
 {
 private:
 	// Variables //
-
+	
+	float mXPos;		// X position on screen
+	float mYPos;		// Y position on screen
+	
+	int mHealth;		// How much health remaining this life
+	int mLives;		// How many lives the player has left
 
 public:
 	// Constructor
-	CPlayer();
+	CPlayer(float xPos, float yPos);
 	//  Destructor
 	~CPlayer();
+	
+	// Getters
+	float GetXPos() const { return mXPos; }
+	float GetYPos() const { return mYPos; }
+	
+	// Setters
+	void SetXPos(float xPos) { mXPos = xPos; }
+	void SetYPos(float yPos) { mYPos = yPos; }
 
 private:
 	// Private Methods //
