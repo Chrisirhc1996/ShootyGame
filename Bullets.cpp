27950b0ea44be1bf79bff3a55c48e8f662b6917a
AJWalker97 <alwalker97@aol.com>
2017-01-24 20:36:30 +0000

    Update Bullets.cpp

--- Bullets.cpp
+++ Bullets.cpp
@@ -9,13 +9,30 @@
 //---- Public Methods ---------------------------------------------------------
 //-----------------------------------------------------------------------------
 
-CBullets::CBullets()
+//////////////////////BULLET.CPP
+
+CBullets::CBullets(tle::I3DEngine* mpMyEngine, tle::IMesh* mMesh, string type, float mLifetime, float mSpeed, float mRadius)
 {
+	if (type == "particle")
+	{
+		mModel = mMesh->CreateModel(PLAYER_POS_X, PLAYER_POS_Y, PLAYER_POS_Z);
+		mLifetime = BULLET_LIFETIME;
+		mSpeed = BULLET_SPEED;
+		mRadius = BULLET_RADIUS;
+	}
 }
 
 
 CBullets::~CBullets()
 {
+}
+
+void CBullets::MoveBullet(float frameTime)
+{
+	mModel->MoveX(mSpeed * frameTime);
+	mLifetime -= frameTime;
+
+
 }
 
 
